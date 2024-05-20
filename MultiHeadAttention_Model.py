06faf122d489be86c3dec126736a6d30f01e6be7
oliverlee000 <oliver.lee@stanford.edu>
2024-05-20 08:40:35 -0700

    weighted threshold by seq_length such that alpha =1 should ead to no words attended to

--- MultiHeadAttention_Model.py
+++ MultiHeadAttention_Model.py
@@ -17,7 +17,7 @@ def create_attention_mask(attentions, threshold):
     batch_size, num_heads, seq_length = attentions[0].size(0), attentions[0].size(1), attentions[0].size(2)
     attentions_np = np.stack([attn.detach().cpu().numpy() for attn in attentions])
     sums = np.sum(attentions_np, axis=3)
-    masks = np.where(sums < threshold, 0, 1)
+    masks = np.where(sums < threshold * seq_length, 0, 1)
     attention_masks = np.repeat(masks[:, :, :, np.newaxis], seq_length, axis=3)
     print("Attention masks created.")
     return torch.from_numpy(attention_masks).to(attentions[0].device)
